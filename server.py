58baa3a54d2a52da7799239e836ec170fc0c6ff0
Diana <dflee805@gmail.com>
2016-09-06 15:14:54 -0700

    add basic oauth and install extra reqs

--- server.py
+++ server.py
@@ -4,6 +4,58 @@ from flask_debugtoolbar import DebugToolbarExtension
 # add tables from model.py when ready
 from model import connect_to_db, db
 import os
+from rauth.service import OAuth1Service, OAuth1Session
+
+CONSUMER_KEY = os.environ['GOODREADS_KEY']
+CONSUMER_SECRET = os.environ['GOODREADS_SECRET']
+
+
+def authorize_user():
+    """This uses goodreads oauth to authorize user into app."""   
+    goodreads = OAuth1Service(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        name='goodreads',
+        request_token_url='http://www.goodreads.com/oauth/request_token',
+        authorize_url='http://www.goodreads.com/oauth/authorize',
+        access_token_url='http://www.goodreads.com/oauth/access_token',
+        base_url='http://www.goodreads.com/'
+        )
+
+    request_token, request_token_secret = goodreads.get_request_token(header_auth=True)
+
+    authorize_url = goodreads.get_authorize_url(request_token)
+    print 'Visit this URL in your browser: ' + authorize_url
+    accepted = 'n'
+    while accepted.lower() == 'n':
+        # you need to access the authorize_link via a browser,
+        # and proceed to manually authorize the consumer
+        accepted = raw_input('Have you authorized me? (y/n) ')
+    print accepted
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 app = Flask(__name__)
@@ -32,14 +84,14 @@ def index():
 
 
 
-if __name__ == "__main__":
-    # We have to set debug=True here, since it has to be True at the point
-    # that we invoke the DebugToolbarExtension
-    app.debug = True
+# if __name__ == "__main__":
+#     # We have to set debug=True here, since it has to be True at the point
+#     # that we invoke the DebugToolbarExtension
+#     app.debug = True
 
-    connect_to_db(app)
+#     connect_to_db(app)
 
-    # Use the DebugToolbar
-    DebugToolbarExtension(app)
+#     # Use the DebugToolbar
+#     DebugToolbarExtension(app)
 
-    app.run()
+#     app.run()
